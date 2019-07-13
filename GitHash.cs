8a6e1270692f79391d4766efcee1221db9764ac0
Anton Chizhov <anton@chizhov.com>
2019-07-14 01:52:39 +0300

    Updated project file for memoQ

--- GitHash.cs
+++ GitHash.cs
@@ -1,5 +1,5 @@
 namespace Intento.MT.Plugin.PropertiesForm { 
    static internal class GitHash    {    
        public const string hash =           
-"876e140708033d831fbefbaa9ceb27549ebbbecd"                                   
+"971d60ec8c6259a1c40c98af9297fbce34b4d7a9"                                   
            ;}}                               
