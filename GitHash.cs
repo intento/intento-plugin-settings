c503db5a2698a99a2d1c9b5722dbc535068a405e
Anton Chizhov <anton@chizhov.com>
2019-07-14 20:39:42 +0300

    Delete GitHash.cs

--- GitHash.cs
+++
@@ -1,5 +0,0 @@
-namespace Intento.MT.Plugin.PropertiesForm { 
-   static internal class GitHash    {    
-       public const string hash =           
-"971d60ec8c6259a1c40c98af9297fbce34b4d7a9"                                   
-           ;}}                               
