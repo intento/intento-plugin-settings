12a5339c5069c0ab0b18d5e516aede513406c8eb
dmitry-tveryanovich <52835174+dmitry-tveryanovich@users.noreply.github.com>
2021-03-03 15:27:31 +0300

    version up

--- RevisionHistory.cs
+++ RevisionHistory.cs
@@ -117,6 +117,7 @@ namespace Intento.MT.Plugin.PropertiesForm
 		// - Bug fix in provider filter for multiple language pairs	
 		// 2.2.21 2021-02-04
 		// - Fixed a bug with configuring a provider with one language pair	
-
+		// 2.2.22 2021-03-02
+		// - added ConsoleUrl option to FormOptions to show different console URL
 	}
 }
