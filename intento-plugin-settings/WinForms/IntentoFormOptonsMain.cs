8e05df67b8395b664c910cfd8103602aaf58a405
Vladimir Abramov <mr.vovan@gmail.com>
2021-03-19 10:41:58 +0300

    Fix: not saving api key until check and uncheck smart routing
    Add: smart default routing when setting up for the first time

--- intento-plugin-settings/WinForms/IntentoFormOptonsMain.cs
+++ intento-plugin-settings/WinForms/IntentoFormOptonsMain.cs
@@ -138,6 +138,11 @@ namespace Intento.MT.Plugin.PropertiesForm
 				}
 
 			currentOptions = originalOptions.Duplicate();
+
+			//smart routing by default
+			if (currentOptions.ApiKey == null && currentOptions.ProviderId == null)
+				currentOptions.SmartRouting = true;
+
 			TraceEndTime = originalOptions.TraceEndTime;
 			formAdvanced = new IntentoFormAdvanced(this);
 			formApi = new IntentoFormOptionsAPI(this);
@@ -475,6 +480,7 @@ namespace Intento.MT.Plugin.PropertiesForm
             if (formApi.DialogResult == DialogResult.OK && apiKeyState.IsOK && apiKeyState.apiKey != apiKey)
             {
                 settingsIsSet = true;
+				currentOptions.ApiKey = apiKeyState.apiKey;
                 RefreshFormInfo();
             }
         }
