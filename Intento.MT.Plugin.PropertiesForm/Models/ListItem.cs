a10093eced96a491d5bb7aea307e4e955043aa13
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2021-12-20 21:50:44 +0400

    CON-1873 Trados: difference between default account and own account

---
+++ Intento.MT.Plugin.PropertiesForm/Models/ListItem.cs
@@ -0,0 +1,42 @@
+﻿namespace Intento.MT.Plugin.PropertiesForm.Models
+{
+    /// <summary>
+    /// Item of list
+    /// </summary>
+    internal class ListItem
+    {
+        /// <summary>
+        /// Display name of item
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Value for item
+        /// </summary>
+        public string Value { get; set; }
+        
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return DisplayName ?? base.ToString();
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ListItem item))
+                return false;
+            return ReferenceEquals(this, obj) || Equals(item);
+        }
+
+        private bool Equals(ListItem other)
+        {
+            return Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Value != null ? Value.GetHashCode() : 0);
+        }
+    }
+}
