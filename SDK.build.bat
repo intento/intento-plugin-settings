1bd6fe5dfdb8228afc118fb32b56058b3b12402c
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-03-21 23:54:42 +0400

    Update SDK package

--- SDK.build.bat
+++ SDK.build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set Configuration=Debug
+set Configuration=Release
 set DoSign=1
-set Version=3.0.26-beta
+set Version=3.0.27-beta
 
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" SDK.build.proj -maxcpucount:1 /fileLogger
 
