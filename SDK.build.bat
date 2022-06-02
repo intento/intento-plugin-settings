4bb9ddaa3b34fad679b851b00a2a47298745bc07
Roman Akhmedyanov <roman.akhmedyanov@inten.to>
2022-06-02 09:59:38 +0400

    Fix build for 2022

--- SDK.build.bat
+++ SDK.build.bat
@@ -4,6 +4,6 @@ set Configuration=Release
 set DoSign=1
 set Version=3.2.3
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" SDK.build.proj -maxcpucount:1 /fileLogger
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe" SDK.build.proj -maxcpucount:1 /fileLogger
 
 pause
